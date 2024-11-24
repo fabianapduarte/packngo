f9dca2a1409bb6e5930fba4b1a07ec953c397327
Fabiana Pereira <fabianaduarte980@gmail.com>
2024-11-24 01:30:30 -0300

    feat: add responsiveness to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,51 @@
-import { Menu } from 'react-feather'
+import { Menu, X } from 'react-feather'
 import { Logotype } from './Logotype'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const Link = ({ text, url }) => {
+const NavbarLink = ({ text, url }) => {
   return (
-    <a href={url} className="font-semibold text-1xl mb-0 hover:text-primary transition-colors">
+    <Link to={url} className="font-semibold text-1xl mb-0 hover:text-primary transition-colors">
       {text}
-    </a>
+    </Link>
   )
 }
 
 export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const handleClickOnMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
-    <header className="flex justify-between items-center px-10 md:px-40 py-6 bg-foreground border-b border-gray">
-      <Logotype />
-      <nav className="hidden md:flex gap-8">
-        <Link text="Viagens" url="/" />
-        <Link text="Conta" url="/" />
-        <Link text="Sair" url="/" />
-      </nav>
-      <Menu className="flex md:hidden cursor-pointer" size={24} color="#BCC1BA" />
-    </header>
+    <nav className="px-10 md:px-20 lg:px-28 xl:px-40 py-6 bg-foreground border-b border-gray">
+      <div className="flex justify-between items-center">
+        <Logotype />
+        <div className="hidden sm:flex gap-8">
+          <NavbarLink text="Viagens" url="/home" />
+          <NavbarLink text="Conta" url="/minha-conta" />
+          <NavbarLink text="Sair" url="/" />
+        </div>
+        {isMenuOpen && (
+          <X className="flex sm:hidden cursor-pointer" size={24} color="#BCC1BA" onClick={handleClickOnMenu} />
+        )}
+        {!isMenuOpen && (
+          <Menu className="flex sm:hidden cursor-pointer" size={24} color="#BCC1BA" onClick={handleClickOnMenu} />
+        )}
+      </div>
+
+      <div
+        className={
+          isMenuOpen ? 'sm:hidden absolute left-0 bg-foreground w-full px-10 pb-6 border-b border-gray' : 'hidden'
+        }
+      >
+        <div className="mt-8 flex flex-col gap-5">
+          <NavbarLink text="Viagens" url="/home" />
+          <NavbarLink text="Conta" url="/minha-conta" />
+          <NavbarLink text="Sair" url="/" />
+        </div>
+      </div>
+    </nav>
   )
 }
