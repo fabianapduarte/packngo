238225a7a0d726a6aee73fb610970b9b34d2775f
Fabiana Pereira <fabianaduarte980@gmail.com>
2024-11-24 02:55:17 -0300

    fix: input color and htmlFor

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,14 +1,15 @@
 export const Input = ({ id, value, onChange, label, type, required }) => {
   return (
     <div className="flex flex-col gap-1">
-      <label for={id}>{label}</label>
+      <label htmlFor={id}>{label}</label>
       <input
         name={id}
         type={type}
         value={value}
         onChange={onChange}
         required={required}
-        className="rounded border border-gray focus:border-secondary px-3 py-2 bg-white" />
+        className="rounded border border-gray focus:ring-secondary focus:border-secondary px-3 py-2 bg-white"
+      />
     </div>
   )
 }
