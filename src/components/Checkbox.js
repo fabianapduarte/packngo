ea06a8a5bff83869c427aafeb9543948c8987755
Fabiana Pereira <fabianaduarte980@gmail.com>
2024-11-24 17:08:42 -0300

    fix: checkbox style and bug on change

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -4,7 +4,7 @@ export const Checkbox = ({ name, text, isChecked, onChange }) => {
   const [checkedValue, setCheckedValue] = useState(isChecked)
 
   const handleCheck = (e) => {
-    onChange(e)
+    if (onChange) onChange(e)
     setCheckedValue(!checkedValue)
   }
 
@@ -12,7 +12,7 @@ export const Checkbox = ({ name, text, isChecked, onChange }) => {
     <div className="flex gap-2 items-center">
       <input
         type="checkbox"
-        className="size-4 rounded-sm border border-gray text-primary focus:ring-primary"
+        className="size-4 rounded-sm border border-gray text-primary focus:ring-primary cursor-pointer"
         name={name}
         checked={checkedValue}
         onChange={handleCheck}
