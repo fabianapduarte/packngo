f1dd6eb8fa9fa092182794f348c7d6ec43a73325
Fabiana Pereira <fabianaduarte980@gmail.com>
2024-11-25 02:28:21 -0300

    fix: home layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,15 @@
-import { LogIn, Plus } from 'react-feather';
-import { Navbar, Button } from '../../components'
+import { LogIn, Plus } from 'react-feather'
+import { Button, Layout } from '../../components'
 import { TravelCard } from '../../components/TravelCard'
 import { enumButtonColor } from '../../enums/enumButtonColor'
+import { enumTravelStatus } from '../../enums/enumTravelStatus'
 
 import './styles.css'
 
-
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <div className="bg-gray lg:items-center min-h-screen px-20 pt-5">
+    <Layout>
+      <div className="lg:items-center">
         <div className="flex items-center justify-between mb-7">
           <div className="font-bold text-2xl">Minhas viagens</div>
           <div className="flex space-x-4">
@@ -18,13 +17,8 @@ export default function Home() {
             <Button label="Criar viagem" color={enumButtonColor.primary} type="submit" Icon={Plus} />
           </div>
         </div>
-        <TravelCard
-          name="Minha Viagem de Teste"
-          status="1"
-          date="01/01/2025"
-          location="Paris"
-        />
+        <TravelCard name="Minha Viagem de Teste" status={enumTravelStatus.planned} date="01/01/2025" location="Paris" />
       </div>
-    </div>
+    </Layout>
   )
 }
