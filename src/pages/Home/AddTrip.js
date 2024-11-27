1ff580700f71172ffb77c3652ae614b44bf843e4
Fabiana Pereira <fabianaduarte980@gmail.com>
2024-11-27 13:52:07 -0300

    refactor: join trip and add trip modal

--- src/pages/Home/AddTrip.js
+++ src/pages/Home/AddTrip.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Plus, Image } from 'react-feather'
-import { Button, Input } from '../../components'
+import { Button, Input, Modal } from '../../components'
 import { enumButtonColor } from '../../enums/enumButtonColor'
 import './styles.css'
 
@@ -52,74 +52,66 @@ export default function AddTrip({ show, onClose, onAddTrip, trips }) {
   }
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-1">
-      <div className="max-w-lg mx-auto bg-white shadow-md rounded p-6 relative">
-        <div className="flex justify-between items-center mb-4">
-          <h1 className="text-xl font-bold">Criar nova viagem</h1>
-          <button onClick={onClose} className="text-gray-500">
-            X
-          </button>
+    <Modal title="Criar nova viagem" size="sm" onClose={onClose}>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <Input
+            value={title}
+            id="title"
+            onChange={(e) => setTitle(e.target.value)}
+            label="Título"
+            type="text"
+            required
+          />
         </div>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
+        <div className="mb-4">
+          <Input
+            value={destination}
+            id="destination"
+            onChange={(e) => setDestination(e.target.value)}
+            label="Destino"
+            type="text"
+            required
+          />
+        </div>
+        <div className="mb-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div>
+            <label className="block text-gray-700 mb-2">Data de Início</label>
             <Input
-              value={title}
-              id="title"
-              onChange={(e) => setTitle(e.target.value)}
-              label="Título"
-              type="text"
+              type="date"
+              id="dateStart"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
               required
             />
           </div>
-          <div className="mb-4">
-            <Input
-              value={destination}
-              id="destination"
-              onChange={(e) => setDestination(e.target.value)}
-              label="Destino"
-              type="text"
-              required
-            />
+          <div>
+            <label className="block text-gray-700 mb-2">Data de Fim</label>
+            <Input type="date" id="dateEnd" value={endDate} onChange={(e) => setEndDate(e.target.value)} required />
           </div>
-          <div className="mb-4 grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div>
-              <label className="block text-gray-700 mb-2">Data de Início</label>
-              <Input
-                type="date"
-                id="dateStart"
-                value={startDate}
-                onChange={(e) => setStartDate(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-gray-700 mb-2">Data de Fim</label>
-              <Input type="date" id="dateEnd" value={endDate} onChange={(e) => setEndDate(e.target.value)} required />
-            </div>
+        </div>
+        <div className="mb-4">
+          <div className="flex items-center space-x-4">
+            <img src={imagePreview} alt="Pré-visualização" className="w-10 h-10 object-cover rounded" />
+            <label
+              htmlFor="imageInput"
+              className="flex items-center cursor-pointer text-primary border border-primary hover:bg-primary hover:text-primary-foreground rounded px-4 py-2 w-fit"
+            >
+              {Image && (
+                <span className="mr-2">
+                  <Image size={16} />
+                </span>
+              )}
+              Alterar Imagem
+            </label>
+            <input id="imageInput" type="file" accept="image/*" onChange={handleImageChange} className="hidden" />
           </div>
-          <div className="mb-4">
-            <div className="flex items-center space-x-4">
-              <img src={imagePreview} alt="Pré-visualização" className="w-10 h-10 object-cover rounded" />
-              <label
-                htmlFor="imageInput"
-                className="flex items-center cursor-pointer text-primary border border-primary hover:bg-primary hover:text-primary-foreground rounded px-4 py-2 w-fit"
-              >
-                {Image && (
-                  <span className="mr-2">
-                    <Image size={16} />
-                  </span>
-                )}
-                Alterar Imagem
-              </label>
-              <input id="imageInput" type="file" accept="image/*" onChange={handleImageChange} className="hidden" />
-            </div>
-          </div>
-          <div className="flex justify-end space-x-4">
-            <Button label="Cancelar" color={enumButtonColor.transparentPrimary} type="button" onClick={onClose} />
-            <Button label="Cadastrar" color={enumButtonColor.primary} type="submit" Icon={Plus} />
-          </div>
-        </form>
-      </div>
-    </div>
+        </div>
+        <div className="flex justify-end space-x-4">
+          <Button label="Cancelar" color={enumButtonColor.transparentPrimary} type="button" onClick={onClose} />
+          <Button label="Cadastrar" color={enumButtonColor.primary} type="submit" Icon={Plus} />
+        </div>
+      </form>
+    </Modal>
   )
 }
--- src/pages/Home/JoinTrip.js
+++ src/pages/Home/JoinTrip.js
@@ -1,151 +1,142 @@
-import React, { useEffect, useState } from 'react';
-import { LogIn, Calendar, MapPin, Check } from 'react-feather';
-import { TravelStatus } from '../../components/TravelStatus';
-import { Button, Input } from '../../components';
-import { enumButtonColor } from '../../enums/enumButtonColor';
-import { enumTravelStatus } from '../../enums/enumTravelStatus';
-import "./styles.css";
+import React, { useState } from 'react'
+import { LogIn, Calendar, MapPin, Check } from 'react-feather'
+import { TravelStatus } from '../../components/TravelStatus'
+import { Button, Input, Modal } from '../../components'
+import { enumButtonColor } from '../../enums/enumButtonColor'
+import { enumTravelStatus } from '../../enums/enumTravelStatus'
+import './styles.css'
+import { useSnackbar } from 'notistack'
 
 export default function JoinTrip({ show, onClose, onJoinTrip, trips, users }) {
-  const [tripUsers, setTripUsers] = useState(0);
-  const [step, setStep] = useState(1);
-  const [inviteCode, setInviteCode] = useState('');
-  const [trip, setTrip] = useState(null);
+  const [tripUsers, setTripUsers] = useState(0)
+  const [step, setStep] = useState(1)
+  const [inviteCode, setInviteCode] = useState('')
+  const [trip, setTrip] = useState(null)
+  const { enqueueSnackbar } = useSnackbar()
 
   if (!show) {
-    return null;
+    return null
   }
 
   const handleNext = () => {
-    const found = trips.find((t) => t.inviteCode === inviteCode);
+    const found = trips.find((t) => t.inviteCode === inviteCode)
     if (found) {
-      setTrip(found);
-      setStep(step + 1);
-      setTripUsers(getTripUsers(found));
+      setTrip(found)
+      setStep(step + 1)
+      setTripUsers(getTripUsers(found))
     } else {
-      alert("Viagem não encontrada. Por favor, verifique o código e tente novamente.");
+      enqueueSnackbar('Viagem não encontrada. Por favor, verifique o código e tente novamente.', { variant: 'error' })
     }
-  };
+  }
 
   const handlePrev = () => {
-    setStep(step - 1);
-    setInviteCode("");
-    setTrip(null);
-  };
+    setStep(step - 1)
+    setInviteCode('')
+    setTrip(null)
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     if (trip) {
-      onJoinTrip(trip);
-      onClose();
-      handlePrev();
+      onJoinTrip(trip)
+      onClose()
+      handlePrev()
     }
-  };
-
+  }
 
   const dateFormat = (dateStart, dateEnd) => {
-    return new Date(dateStart).toLocaleDateString() + " - " + new Date(dateEnd).toLocaleDateString();
-  };
+    return new Date(dateStart).toLocaleDateString() + ' - ' + new Date(dateEnd).toLocaleDateString()
+  }
 
   const statusFormat = (status) => {
     switch (status) {
       case 0:
-        return enumTravelStatus.planned;
+        return enumTravelStatus.planned
       case 1:
-        return enumTravelStatus.progress;
+        return enumTravelStatus.progress
       case 2:
-        return enumTravelStatus.finished;
+        return enumTravelStatus.finished
       default:
-        return "Desconhecido";
+        return 'Desconhecido'
     }
-  };
+  }
 
   const getTripUsers = (trip) => {
-    if (!trip || !users) return [];
-    return users.filter(user => user.activeTrips.includes(trip.id));
-  };
+    if (!trip || !users) return []
+    return users.filter((user) => user.activeTrips.includes(trip.id))
+  }
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-1">
-      <div className="max-w-lg mx-auto bg-white shadow-md rounded p-6 relative">
-        <form onSubmit={handleSubmit}>
-          {step === 1 && (
-            <div>
-              <div className="flex justify-between items-center mb-4">
-                <h1 className="text-xl font-bold">Entrar em uma viagem</h1>
-                <button type="button" onClick={onClose} className="text-gray-500">X</button>
-              </div>
-              <div className="mb-4">
-                <Input
-                  value={inviteCode}
-                  id="inviteCode"
-                  onChange={(e) => setInviteCode(e.target.value)}
-                  label="Código"
-                  type="text"
-                  required
-                />
-              </div>
-              <div className="flex justify-end space-x-4">
-                <Button label="Cancelar" color={enumButtonColor.transparentPrimary} type="button" onClick={onClose} />
-                <Button Icon={LogIn} label="Entrar" onClick={handleNext} color={enumButtonColor.primary} type="button" />
-              </div>
+    <Modal size="md" title={step === 1 ? 'Entrar em uma viagem' : `Deseja entrar em ${trip.title}?`} onClose={onClose}>
+      <form onSubmit={handleSubmit}>
+        {step === 1 && (
+          <>
+            <div className="mb-4">
+              <Input
+                value={inviteCode}
+                id="inviteCode"
+                onChange={(e) => setInviteCode(e.target.value)}
+                label="Código"
+                type="text"
+                required
+              />
             </div>
-          )}
-          {step === 2 && trip && (
-            <div>
-              <div className="flex justify-between items-center mb-4">
-                <h1 className="text-xl font-bold">Deseja entrar em {trip.title}?</h1>
-                <button type="button" onClick={onClose} className="text-gray-500">X</button>
-              </div>
-              <div className="mb-4">
-                <div className="max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl overflow-hidden bg-white my-4 rounded min-h-36 h-full">
-                  <div className="grid grid-cols-1 lg:grid-cols-3 min-h-full">
-                    <div className="relative overflow-hidden object-cover min-w-[175px] min-h-[175px] rounded col-span-1">
-                      <img src={trip.image} alt={trip.imageAlt} className="w-full h-full object-cover" />
+            <div className="flex justify-end space-x-4">
+              <Button label="Cancelar" color={enumButtonColor.transparentPrimary} type="button" onClick={onClose} />
+              <Button Icon={LogIn} label="Entrar" onClick={handleNext} color={enumButtonColor.primary} type="button" />
+            </div>
+          </>
+        )}
+        {step === 2 && trip && (
+          <>
+            <div className="mb-4">
+              <div className="max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl overflow-hidden bg-white my-4 rounded min-h-36 h-full">
+                <div className="grid grid-cols-1 lg:grid-cols-3 min-h-full">
+                  <div className="relative overflow-hidden object-cover min-w-[175px] min-h-[175px] rounded col-span-1">
+                    <img src={trip.image} alt={trip.imageAlt} className="w-full h-full object-cover" />
+                  </div>
+                  <div className="px-6 py-4 col-span-2 my-auto ml-5">
+                    <div className="pl-0.5">
+                      <TravelStatus status={statusFormat(trip.status)} />
                     </div>
-                    <div className="px-6 py-4 col-span-2 my-auto ml-5">
-                      <div className="pl-0.5">
-                        <TravelStatus status={statusFormat(trip.status)} />
-                      </div>
-                      <p className="text-gray-700 text-base flex items-center pr-3 w-fit">
-                        <span className="mr-2">
-                          <MapPin size={16} />
-                        </span>
-                        {trip.destination}
-                      </p>
-                      <p className="text-gray-700 text-base flex items-center pr-3 w-fit">
-                        <span className="mr-2">
-                          <Calendar size={16} />
-                        </span>
-                        {dateFormat(trip.dateStart, trip.dateEnd)}
-                      </p>
-                      <div className="flex mt-2">
-                        {tripUsers.slice(0, 3).map((participant) => (
-                          <img
-                            key={participant.id}
-                            src={participant.image}
-                            alt={participant.name}
-                            className="w-10 h-10 rounded-full border-2 border-white -ml-2 first:ml-0"
-                          />
-                        ))}
-                        {tripUsers && tripUsers.length > 3 && (
-                          <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-200 -ml-2">
-                            +{tripUsers.length - 3} participantes
-                          </div>
-                        )}
-                      </div>
+                    <p className="text-gray-700 text-base flex items-center pr-3 w-fit">
+                      <span className="mr-2">
+                        <MapPin size={16} />
+                      </span>
+                      {trip.destination}
+                    </p>
+                    <p className="text-gray-700 text-base flex items-center pr-3 w-fit">
+                      <span className="mr-2">
+                        <Calendar size={16} />
+                      </span>
+                      {dateFormat(trip.dateStart, trip.dateEnd)}
+                    </p>
+                    <div className="flex mt-2">
+                      {tripUsers.slice(0, 3).map((participant) => (
+                        <img
+                          key={participant.id}
+                          src={participant.image}
+                          alt={participant.name}
+                          className="w-10 h-10 rounded-full border-2 border-white -ml-2 first:ml-0"
+                        />
+                      ))}
+                      {tripUsers && tripUsers.length > 3 && (
+                        <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-200 -ml-2">
+                          +{tripUsers.length - 3} participantes
+                        </div>
+                      )}
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="flex justify-end space-x-4">
-                <Button label="Cancelar" color={enumButtonColor.transparentPrimary} type="button" onClick={handlePrev} />
-                <Button Icon={Check} label="Confirmar" color={enumButtonColor.primary} type="submit" />
-              </div>
             </div>
-          )}
-        </form>
-      </div>
-    </div>
-  );
+            <div className="flex justify-end space-x-4">
+              <Button label="Cancelar" color={enumButtonColor.transparentPrimary} type="button" onClick={handlePrev} />
+              <Button Icon={Check} label="Confirmar" color={enumButtonColor.primary} type="submit" />
+            </div>
+          </>
+        )}
+      </form>
+    </Modal>
+  )
 }
