dc916c478d14ed09b92b196e0474d67ee1aa197b
Fabiana Pereira <fabianaduarte980@gmail.com>
2025-01-13 22:05:09 -0300

    refactor: api call functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,50 +2,45 @@ import axios from 'axios'
 import { baseUrl } from './routesApi'
 import { cookies } from './cookies'
 
-export const post = (url, data) => {
-  const fullUrl = baseUrl + url
+const getHeaders = () => {
   const token = cookies.get('token')
 
-  return axios.post(fullUrl, data, {
+  return {
     headers: {
       Accept: 'application/json',
       Authorization: `Bearer ${token}`,
     },
-  })
+  }
+}
+
+const getFullUrl = (url) => {
+  return baseUrl + url
+}
+
+export const post = (url, data) => {
+  const fullUrl = getFullUrl(url)
+  const headers = getHeaders()
+
+  return axios.post(fullUrl, data, headers)
 }
 
 export const get = (url) => {
-  const fullUrl = baseUrl + url
-  const token = cookies.get('token')
+  const fullUrl = getFullUrl(url)
+  const headers = getHeaders()
 
-  return axios.get(fullUrl, {
-    headers: {
-      Accept: 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  return axios.get(fullUrl, headers)
 }
 
 export const patch = (url, data) => {
-  const fullUrl = baseUrl + url
-  const token = cookies.get('token')
+  const fullUrl = getFullUrl(url)
+  const headers = getHeaders()
 
-  return axios.patch(fullUrl, data, {
-    headers: {
-      Accept: 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  return axios.patch(fullUrl, data, headers)
 }
 
 export const del = (url) => {
-  const fullUrl = baseUrl + url
-  const token = cookies.get('token')
+  const fullUrl = getFullUrl(url)
+  const headers = getHeaders()
 
-  return axios.delete(fullUrl, {
-    headers: {
-      Accept: 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  return axios.delete(fullUrl, headers)
 }
