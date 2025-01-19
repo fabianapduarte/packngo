d666da00bc3a91739fd4df361e5c7e20948c98c5
Fabiana Pereira <fabianaduarte980@gmail.com>
2025-01-19 19:19:11 -0300

    fix: home layout

--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -7,14 +7,14 @@ import { getTripImage } from '../utils/getTripImage'
 
 export const TravelCard = ({ id, name, status, date, location, image, imageAlt = 'Placeholder' }) => {
   return (
-    <div className="bg-white rounded h-64 lg:h-56 w-full">
+    <div className="bg-white rounded h-64 lg:h-36 w-full">
       <Link to={tripRoute(id)}>
         <div className="grid grid-cols-1 lg:grid-cols-3 h-full">
           <div className="relative overflow-hidden min-w-[100px] rounded-t lg:rounded-l lg:rounded-tr-none col-span-1">
             <img src={getTripImage(image)} alt={imageAlt} className="w-full h-full object-cover z-0" />
           </div>
-          <div className="px-6 py-4 col-span-2 my-auto">
-            <div className="font-bold text-xl mb-2">{name}</div>
+          <div className="p-4 col-span-2 my-auto">
+            <div className="font-bold text-xl mb-2 line-clamp-1">{name}</div>
             <div className="pl-0.5">
               <TravelStatus status={status} />
             </div>
